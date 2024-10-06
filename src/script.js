5b35befb4e732312a8d19e11cf2922d2c5a41c0d
Lyubo33 <lyubcho50@gmail.com>
2024-10-07 01:26:17 +0300

    fixing bug with mobile layout

--- src/script.js
+++ src/script.js
@@ -18,7 +18,7 @@ function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".menu-icon");
 
-    menu.classList.toggle("open")
+    menu.classList.toggle("open");
     icon.classList.toggle("open");
 
 }
