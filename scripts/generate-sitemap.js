e545837be6bed427a7712e1dc96acba30df2d05d
Lyubo33 <lyubcho50@gmail.com>
2025-06-05 16:36:11 +0300

    removing section links

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -4,18 +4,11 @@ const path = require('path');
 
 const baseUrl = 'https://lyubos-portfolio.vercel.app';
 const links = [
-  {url: '/', changefreq: 'weekly', priority: 1.0},
-  {url: '/#about', changefreq: 'monthly', priority: 0.8},
-  {url: '/#skills-education', changefreq: 'monthly', priority: 0.8},
-  {url: '/#projects', changefreq: 'monthly', priority: 0.8},
-  {url: '/#contacts', changefreq: 'monthly', priority: 0.8},
+  {url: '/', changefreq: 'weekly', priority: 1.0}
 ];
 
 async function generateSitemap() {
-  const sitemapStream = new SitemapStream({
-    hostname: baseUrl, 
-    xmlns: true,
-    });
+  const sitemapStream = new SitemapStream({hostname: baseUrl});
   const outputPath = path.join(__dirname, '../dist/sitemap.xml');
   const writeStream = createWriteStream(outputPath);
   sitemapStream.pipe(writeStream);
